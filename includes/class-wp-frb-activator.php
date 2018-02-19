83f00ffc276e2b27a59b787948966b8227a03fee
Stephan Gensch <stgensch@vragments.com>
2018-02-19 08:16:09 +0100

    initial boilerplate

---
+++ includes/class-wp-frb-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       netzwerkcafe.org
+ * @since      1.0.0
+ *
+ * @package    Wp_Frb
+ * @subpackage Wp_Frb/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Frb
+ * @subpackage Wp_Frb/includes
+ * @author     Stephan Gensch <stephan@vragments.com>
+ */
+class Wp_Frb_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-frb-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       netzwerkcafe.org
+ * @since      1.0.0
+ *
+ * @package    Wp_Frb
+ * @subpackage Wp_Frb/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Frb
+ * @subpackage Wp_Frb/includes
+ * @author     Stephan Gensch <stephan@vragments.com>
+ */
+class Wp_Frb_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
